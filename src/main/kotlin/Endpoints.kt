4df442b55312b0b15247170ccc62c4fbe477b455
Paul Martin <paul.y.martin@johnlewis.co.uk>
2019-11-04 09:38:09 +0000

    Increment hello no.

--- src/main/kotlin/Endpoints.kt
+++ src/main/kotlin/Endpoints.kt
@@ -16,7 +16,7 @@ internal fun endpoints(pubCrawlUri: String): HttpHandler {
     return ServerFilters.CatchLensFailure
         .then(
             routes(
-                "/" bind GET to { Response(OK).body("Hello 5") },
+                "/" bind GET to { Response(OK).body("Hello 6") },
                 findBeersEndpoint(findBeers)
             )
         )
